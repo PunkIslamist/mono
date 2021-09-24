ea698784d8084b1a367a18d00be2f60ddafdfe77
punkislamist <sebastian.gebert@posteo.de>
2021-09-24 17:44:52 +0200

    Improved JS version for aoc 2.1 a bit

--- advent-of-code/2020/1/solver.js
+++ advent-of-code/2020/1/solver.js
@@ -2,23 +2,19 @@ import { readFileSync } from 'fs';
 import { Seq } from 'immutable';
 import { EOL } from 'os';
 
-/**
- * 
- * @param {Number[]} numbers 
- * @returns Number
- */
-function sum(numbers) { return numbers.reduce((x, y) => x + y) }
+const add = (x, y) => x + y
+const sum = (numbers) => numbers.reduce(add)
 
 const data = readFileSync('expenses.txt', 'utf8')
     .split(EOL)
     .map(x => Number.parseInt(x))
     .sort((a, b) => a - b);
 
-const dat = Seq(data);
-const rev = Seq(dat).reverse();
+const descending = Seq(data);
+const ascending = Seq(descending).reverse();
 console.log(
-    dat.flatMap(x => rev.map(y => [x, y]))
-        .filter(xy => sum(xy) < 2020)
-        .flatMap(xy => rev.map(z => [...xy, z]))
+    descending.flatMap(x => ascending.map(y => [x, y]))
+        .takeWhile(xy => sum(xy) < 2020)
+        .flatMap(xy => ascending.map(z => [...xy, z]))
         .find(xyz => sum(xyz) === 2020)
 )
