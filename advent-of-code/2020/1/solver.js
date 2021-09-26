d19f1d8081140f446500db2b4604e2d2d0cdbd11
punkislamist <sebastian.gebert@posteo.de>
2021-09-26 13:33:29 +0200

    Restored functionality

--- advent-of-code/2020/1/solver.js
+++ advent-of-code/2020/1/solver.js
@@ -10,11 +10,14 @@ const data = readFileSync('expenses.txt', 'utf8')
     .map(x => Number.parseInt(x))
     .sort((a, b) => a - b);
 
-const descending = Seq(data);
-const ascending = Seq(descending).reverse();
+const ascending = Seq(data);
+const descending = Seq(ascending).reverse();
 console.log(
-    descending.flatMap(x => ascending.map(y => [x, y]))
-        .takeWhile(xy => sum(xy) < 2020)
-        .flatMap(xy => ascending.map(z => [...xy, z]))
+    descending.flatMap(x =>
+        ascending.map(y => [x, y])
+            .takeUntil(xy => sum(xy) > 2020))
+        .flatMap(xy =>
+            ascending.map(z => [...xy, z])
+                .takeUntil(xy => sum(xy) > 2020))
         .find(xyz => sum(xyz) === 2020)
 )
