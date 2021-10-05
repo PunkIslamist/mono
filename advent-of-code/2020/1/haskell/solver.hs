a4d16dec90e9eeee2a9ebec81205e7636a241ecd
punkislamist <sebastian.gebert@posteo.de>
2021-10-05 13:30:31 +0200

    AoC 1.2 in Haskell

---
+++ advent-of-code/2020/1/haskell/solver.hs
@@ -0,0 +1,22 @@
+import           Data.List
+
+main = do
+    content <- readFile "../input/expenses.txt"
+    let nums = map readInt $ lines content
+    print $ solve 2020 $ sort nums
+    return ()
+
+readInt :: String -> Int
+readInt = read
+
+solve target numbers = solve' target $ sort numbers
+solve' target numbers = head
+    [ x : yz
+    | yz <-
+        [ [y, z]
+        | z <- reverse numbers
+        , y <- takeWhile (\n -> n + z < target) numbers
+        ]
+    , x <- takeWhile (\n -> sum (n : yz) <= target) numbers
+    , sum (x : yz) == target
+    ]
