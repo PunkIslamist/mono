b2e695f83645f70c212884452f7759c3ede4ccf6
punkislamist <sebastian.gebert@posteo.de>
2021-11-28 12:50:24 +0100

    Grokking Simpicity: added example for deep updates

---
+++ grokking-simplicity/index.js
@@ -0,0 +1,45 @@
+// Chapter 14 - Nested updates
+
+const jinx = {
+    name: 'Powder',
+    nick: 'Jinx',
+    sister: {
+        name: 'Violet',
+        nick: 'Vi'
+    },
+    enemies: [
+        {
+            name: 'Caitlyn',
+            nick: 'Cupcake'
+        },
+        {
+            name: 'Violet',
+            nick: 'Vi'
+        },
+    ]
+}
+
+const pop = xs => ({ head: xs[0], tail: xs.slice(1) })
+
+const update = (obj, prop, f) => {
+    let copy = { ...obj };
+    copy[prop] = f(obj[prop]);
+    return copy;
+}
+
+const deepUpdate = (obj, props, modify) => {
+    if (props.length === 0) {
+        return modify(obj);
+    } else {
+        const { head, tail } = pop(props);
+
+        return update(obj, head, propValue =>
+            deepUpdate(propValue, tail, modify));
+    }
+}
+
+console.log(update(jinx, 'name', _ => 'Lynx'));
+console.log(deepUpdate(jinx, ['name'], n => `Changed ${n}`));
+console.log(deepUpdate(jinx, ['sister', 'name'], n => `Changed ${n}`));
+console.log(deepUpdate(jinx, ['enemies'], (arr => arr.filter(x => x.name != 'Caitlyn'))));
+console.log(jinx);
