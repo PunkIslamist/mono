90423586e3d2f06d5adeb2a39b647bdc35c3fe6e
punkislamist <sebastian.gebert@posteo.de>
2021-11-28 13:21:55 +0100

    Grokking Simpicity: deep updates now also work for objects within arrays

--- grokking-simplicity/index.js
+++ grokking-simplicity/index.js
@@ -21,9 +21,11 @@ const jinx = {
 
 const pop = xs => ({ head: xs[0], tail: xs.slice(1) })
 
-const update = (obj, prop, f) => {
-    let copy = { ...obj };
-    copy[prop] = f(obj[prop]);
+const update = (thing, accessor, f) => {
+    let copy = Array.isArray(thing)
+        ? [...thing]
+        : { ...thing };
+    copy[accessor] = f(copy[accessor]);
     return copy;
 }
 
@@ -42,4 +44,7 @@ console.log(update(jinx, 'name', _ => 'Lynx'));
 console.log(deepUpdate(jinx, ['name'], n => `Changed ${n}`));
 console.log(deepUpdate(jinx, ['sister', 'name'], n => `Changed ${n}`));
 console.log(deepUpdate(jinx, ['enemies'], (arr => arr.filter(x => x.name != 'Caitlyn'))));
+console.log(deepUpdate(jinx, ['enemies', 1, 'name'], n => `Changed ${n}`));
+
+// Ensure we never mutated
 console.log(jinx);
