a8c4e737c97ceb7ef10a2d4c7a918a7884c0345a
punkislamist <sebastian.gebert@posteo.de>
2021-12-04 13:18:33 +0100

    AoC: working solution for 01 in clojure

--- advent-of-code/2021/01/clojure/01.clj
+++ advent-of-code/2021/01/clojure/01.clj
@@ -1,14 +1,21 @@
+(require '[clojure.edn :as edn])
+(require '[clojure.string :as string])
+
 (def example-values [199 200 208 210 200 207 240 269 260 263])
 
 (defn pairs [values]
-  (loop [left values right (next left) current []]
-    (let [[lhead & ltail] left [rhead & rtail] right]
+  (loop [left values, right (next left), current []]
+    (let [[lhead & ltail] left, [rhead & rtail] right]
       (if
         (not rhead) current
         (recur ltail rtail (conj current [lhead rhead]))))))
 
 (defn increasing [values]
-  (let [firstLessThanSecond #(< (first %) (second %))]
-  (count (filter firstLessThanSecond (pairs values)))))
+  (let [first<second #(< (first %) (second %))]
+    (count (filter first<second (pairs values)))))
+
+(def actual-values
+  (map edn/read-string (string/split-lines (slurp "../input.txt"))))
 
 (println "Example count:" (increasing example-values))
+(println "Actual count:" (increasing actual-values))
