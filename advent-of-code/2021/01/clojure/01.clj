b1fb5a47d4502931f06f1650a47d8083c0f2d192
punkislamist <sebastian.gebert@posteo.de>
2021-12-05 10:04:30 +0100

    AoC: finished part 2 of 01

--- advent-of-code/2021/01/clojure/01.clj
+++ advent-of-code/2021/01/clojure/01.clj
@@ -5,23 +5,33 @@
 (def example-values [199 200 208 210 200 207 240 269 260 263])
 
 
-(defn pairs [values]
-  (loop [left    values
-         right   (next left)
-         current []]
-    (if (empty? right) current
-        (recur (next left)
-               (next right)
-               (conj current [(first left) (first right)])))))
-
-(defn increasing [values]
-  (let [first<second (fn [[a b]] (< a b))]
-    (count (filter first<second (pairs values)))))
-
-
 (def actual-values
   (map edn/read-string (string/split-lines (slurp "../input.txt"))))
 
 
-(println "Example count:" (increasing example-values))
-(println "Actual count:" (increasing actual-values))
+(defn windows [window-size values]
+  (loop [remaining  values
+         windows    []]
+    (if (< (count remaining) window-size) windows
+        (recur (next remaining)
+               (conj windows (take window-size remaining))))))
+
+
+(defn increasing [values]
+  (let [first<second (fn [[a b]] (< a b))]
+    (count (filter first<second values))))
+
+
+(defn neighbours [values] (windows 2 values))
+
+
+(defn triplet-sums [values]
+  (let [sum       (partial apply +)
+        triplets  (windows 3 values)]
+    (neighbours (map sum triplets))))
+
+
+(println "First example count:" (increasing (neighbours example-values)))
+(println "First actual count:" (increasing (neighbours actual-values)))
+(println "Second example count:" (increasing (triplet-sums example-values)))
+(println "Second actual count:" (increasing (triplet-sums actual-values)))
