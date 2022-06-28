05a90519a040fd67e1ccb0c116600f81f78950ca
punkislamist <sebastian.gebert@posteo.de>
2022-06-28 20:43:36 +0200

    Working solution for part 1

--- apps/advent-of-code/2021/01/csharp/01/Program.cs
+++ apps/advent-of-code/2021/01/csharp/01/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static IEnumerable<int> depthChanges(IEnumerable<int> sonarValues) =>
+    sonarValues
+        .Zip(sonarValues.Skip(1))
+        .Select(it => it.Second - it.First);
+
+static int depthIncreaseCount(IEnumerable<int> depthDifferences) =>
+    depthDifferences.Count(it => it > 0);
+
+var exampleValues = new[] {
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+};
+
+var sonarValues = File
+    .ReadAllLines("../../input.txt")
+    .Select(int.Parse);
+var resultPart1 = depthIncreaseCount(depthChanges(sonarValues)); // 1374
+
+Console.WriteLine(string.Join(Environment.NewLine, resultPart1));
