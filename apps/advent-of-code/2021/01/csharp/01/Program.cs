2724f12c96ffb101f38ff1da237cae00698ac2cf
punkislamist <sebastian.gebert@posteo.de>
2022-06-28 20:54:49 +0200

    Using extensioon methods

--- apps/advent-of-code/2021/01/csharp/01/Program.cs
+++ apps/advent-of-code/2021/01/csharp/01/Program.cs
@@ -1,27 +1,18 @@
-﻿static IEnumerable<int> depthChanges(IEnumerable<int> sonarValues) =>
-    sonarValues
-        .Zip(sonarValues.Skip(1))
-        .Select(it => it.Second - it.First);
-
-static int depthIncreaseCount(IEnumerable<int> depthDifferences) =>
-    depthDifferences.Count(it => it > 0);
-
-var exampleValues = new[] {
-    199,
-    200,
-    208,
-    210,
-    200,
-    207,
-    240,
-    269,
-    260,
-    263
-};
-
-var sonarValues = File
+﻿var sonarValues = File
     .ReadAllLines("../../input.txt")
     .Select(int.Parse);
-var resultPart1 = depthIncreaseCount(depthChanges(sonarValues)); // 1374
+
+var resultPart1 = sonarValues.depthChanges().depthIncreaseCount();
 
 Console.WriteLine(string.Join(Environment.NewLine, resultPart1));
+
+public static class Extensions
+{
+    public static IEnumerable<int> depthChanges(this IEnumerable<int> sonarValues) =>
+        sonarValues
+            .Zip(sonarValues.Skip(1))
+            .Select(it => it.Second - it.First);
+
+    public static int depthIncreaseCount(this IEnumerable<int> depthDifferences) =>
+        depthDifferences.Count(it => it > 0);
+}
