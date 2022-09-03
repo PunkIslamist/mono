9a173d49640604ff49ba6b054929647f28225367
punkislamist <sebastian.gebert@posteo.de>
2022-09-03 14:49:49 +0200

    Add route for public resources

--- learn/web-development-with-clojure/ring-app/src/ring_app/core.clj
+++ learn/web-development-with-clojure/ring-app/src/ring_app/core.clj
@@ -41,13 +41,15 @@
 (def handler
   (reitit/ring-handler
    (reitit/router routes)
-   (reitit/create-default-handler
-    {:not-found          (constantly
-                          (response/not-found "404 - Page not found"))
-     :method-not-allowed (constantly
-                          (response/method-not-allowed "405 - Not allowed"))
-     :not-acceptable     (constantly
-                          (response/not-acceptable "406 - Not acceptable"))})))
+   (reitit/routes
+    (reitit/create-resource-handler {:path "/"})
+    (reitit/create-default-handler
+     {:not-found          (constantly
+                           (response/not-found "404 - Page not found"))
+      :method-not-allowed (constantly
+                           (response/method-not-allowed "405 - Not allowed"))
+      :not-acceptable     (constantly
+                           (response/not-acceptable "406 - Not acceptable"))}))))
 
 
 (defn -main []
