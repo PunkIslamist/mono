a9536484f554264a6742e331013c8375df3d7aff
punkislamist <sebastian.gebert@posteo.de>
2022-09-03 15:32:42 +0200

    Some good ol' templatin'

--- learn/web-development-with-clojure/html-templating/src/html_templating/core.clj
+++ learn/web-development-with-clojure/html-templating/src/html_templating/core.clj
@@ -1,2 +1,10 @@
 (ns html-templating.core
   (:require [selmer.parser :as selmer]))
+
+(comment
+  (selmer/render "Hello, {{name}}" {:name "Ding dong"})
+  (selmer/render-file "hello.html" {:name "Amy"})
+  (selmer/render-file
+    "hello.html"
+    {:items (range 3)})
+  )
