9cc686e27cf731fae3cd50d23a8ec240518e5b89
punkislamist <sebastian.gebert@posteo.de>
2022-09-05 19:05:59 +0200

    Guestbook: create form with reagent

--- learn/web-development-with-clojure/guestbook/src/cljs/guestbook/core.cljs
+++ learn/web-development-with-clojure/guestbook/src/cljs/guestbook/core.cljs
@@ -2,8 +2,39 @@
   (:require [reagent.core :as r]
             [reagent.dom :as dom]))
 
+  
+(defn message-form []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input {:type      :text
+                       :name      :name
+                       :value     (:name @fields)
+                       :on-change #(swap! fields assoc :name (-> %
+                                                                 .-target
+                                                                 .-value))}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [:textarea.textarea {:name      :message
+                             :value     (:message @fields)
+                             :on-change #(swap! fields assoc :message (-> %
+                                                                          .-target
+                                                                          .-value))}]]
+       [:p "Name: " (:name @fields)]
+       [:p "Message " (:message @fields)]
+       [:input.button.is-primary {:type :submit
+                                  :value "comment"}]])))
 
-(dom/render
- [:div#hello.content
-  [:h1 "Hello, syntactical sugar!"]]
- (.getElementById js/document "content"))
+
+(defn home []
+  [:div.content
+   [:div.columns.is-centered
+    [:div.column.is-two-thirds
+     [:div.columns
+      [:div.column
+       [message-form]]]]]])
+
+
+(dom/render [home] (.getElementById js/document "content"))
