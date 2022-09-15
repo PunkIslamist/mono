7464d356b6845bf1395544ae82de538efd9d8a24
punkislamist <sebastian.gebert@posteo.de>
2022-09-15 19:32:13 +0200

    Split out messages stuff

--- learn/web-development-with-clojure/guestbook/src/clj/guestbook/routes/home.clj
+++ learn/web-development-with-clojure/guestbook/src/clj/guestbook/routes/home.clj
@@ -1,8 +1,7 @@
 (ns guestbook.routes.home
   (:require
    [guestbook.layout :as layout]
-   [guestbook.db.core :as db]
-   [clojure.java.io :as io]
+   [guestbook.messages :as msg]
    [guestbook.middleware :as middleware]
    [ring.util.response]
    [ring.util.http-response :as response]
@@ -22,17 +21,19 @@
 
 
 (defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (response/bad-request {:errors errors})
-    (try
-      (db/save-message! params)
-      (response/ok {:status :ok})
-      (catch Exception _
-        (response/internal-server-error {:errors {:server-error ["Failed to save message :()"]}})))))
+  (try
+    (msg/save-message! params)
+    (response/ok {:status :ok})
+    (catch Exception e
+      (let [{id     :guestbook/error-id
+             errors :errors} (ex-data e)]
+        (case id
+          :validation (response/bad-request {:errors errors})
+          (response/internal-server-error {:errors {:server-error ["Failed to save message!"]}}))))))
 
 
 (defn message-list [_]
-  (response/ok {:messages (vec (db/get-messages))}))
+  (response/ok (msg/message-list)))
 
 
 (defn home-routes []
