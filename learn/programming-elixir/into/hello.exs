378d83c7f61e12923c3e3fa78bd292810e4375e7
punkislamist <sebastian.gebert@posteo.de>
2022-12-23 21:33:24 +0100

    Exercise Function-2 (FizzBuzz)

--- learn/programming-elixir/into/hello.exs
+++ learn/programming-elixir/into/hello.exs
@@ -7,3 +7,14 @@ list_concat = fn a, b -> a ++ b end
 sum = fn x, y, z -> x + y + z end
 
 pair_tuple_to_list = fn {a, b} -> [a, b] end
+
+# Exercise Functions-2
+# Using module and named functions as I cannot send to repl from file :(
+
+defmodule Functions_2 do
+  def fizz_buzz(0, 0, _), do: "FizzBuzz"
+  def fizz_buzz(0, _, _), do: "Fizz"
+  def fizz_buzz(_, 0, _), do: "Buzz"
+  def fizz_buzz(_, _, n), do: n
+  def fizz_buzz(x), do: fizz_buzz(rem(x, 3), rem(x, 5), x)
+end
