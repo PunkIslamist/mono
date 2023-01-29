a118e53d05d4a29b6bcef7191be48b769d619fa9
punkislamist <sebastian.gebert@posteo.de>
2023-01-29 12:25:45 +0100

    Example correct but input wrong??

--- apps/advent-of-code/2022/11/fsharp/11.fsx
+++ apps/advent-of-code/2022/11/fsharp/11.fsx
@@ -3,36 +3,7 @@
 open Basics.Numbers
 open Basics.Collections
 open System.Text.RegularExpressions
-
-let example = [
-    [
-    "Monkey 0:"
-    "  Starting items: 79, 98"
-    "  Operation: new = old * 19"
-    "  Test: divisible by 23"
-    "    If true: throw to monkey 2"
-    "    If false: throw to monkey 3"]
-    [
-    "Monkey 1:"
-    "  Starting items: 54, 65, 75, 74"
-    "  Operation: new = old + 6"
-    "  Test: divisible by 19"
-    "    If true: throw to monkey 2"
-    "    If false: throw to monkey 0"]
-    [
-    "Monkey 2:"
-    "  Starting items: 79, 60, 97"
-    "  Operation: new = old * old"
-    "  Test: divisible by 13"
-    "    If true: throw to monkey 1"
-    "    If false: throw to monkey 3"]
-    [
-    "Monkey 3:"
-    "  Starting items: 74"
-    "  Operation: new = old + 3"
-    "  Test: divisible by 17"
-    "    If true: throw to monkey 0"
-    "    If false: throw to monkey 1"]]
+open Basics.String
 
 type Monkee = {
     Items : int list
@@ -40,23 +11,6 @@ type Monkee = {
     Test: int -> int }
     
 
-let m0 = {
-    Items = [79; 98]
-    Operation = (*) 19
-    Test = fun x -> if x % 23 = 0 then 2 else 3 }
-let m1 = {
-    Items = [54; 64; 75; 74]
-    Operation = (+) 6
-    Test = fun x -> if x % 19 = 0 then 2 else 0 }
-let m2 = {
-    Items = [79; 60; 97]
-    Operation = fun x -> x * x
-    Test = fun x -> if x % 13 = 0 then 1 else 3 }
-let m3 = {
-    Items = [74]
-    Operation = (+) 3
-    Test = fun x -> if x % 17 = 0 then 0 else 1 }
-    
 let singleValue (groupName: string) (reMatch : Match) =
     reMatch
         .Groups[groupName]
@@ -70,21 +24,13 @@ let allValues (groupName: string) (reMatch : Match) =
     |> Seq.map (fun x -> x.Value)
 
 
-let exampleMonkee = [
-    "Monkey 0:"
-    "  Starting items: 79, 98"
-    "  Operation: new = old * 19"
-    "  Test: divisible by 23"
-    "    If true: throw to monkey 2"
-    "    If false: throw to monkey 3"]
-    
-let parseMonkee (lines : string list) =
+let parseMonkee lines =
     let itemsPattern = @"Starting items:( (?<items>\d+),?)+"
     let operationPattern = @"Operation: new = (?<left>old|\d+) (?<op>(\+|\*)) (?<right>old|\d+)"
     let testPattern = @"Test: divisible by (?<divisor>\d+)"
     let targetPattern = @"If \w+: throw to monkey (?<target>\d+)"
 
-    let [_; items; operation; test; ``if true``; ``if false``] = lines
+    let [|_; items; operation; test; ``if true``; ``if false``|] = lines
 
     let items =
         Regex.Match(items, itemsPattern)
@@ -175,9 +121,81 @@ let round monkees =
     |> fun (_, monkees, throws) -> monkees, throws
 
 
+let example = [|
+    [|
+    "Monkey 0:"
+    "  Starting items: 79, 98"
+    "  Operation: new = old * 19"
+    "  Test: divisible by 23"
+    "    If true: throw to monkey 2"
+    "    If false: throw to monkey 3"|]
+    [|
+    "Monkey 1:"
+    "  Starting items: 54, 65, 75, 74"
+    "  Operation: new = old + 6"
+    "  Test: divisible by 19"
+    "    If true: throw to monkey 2"
+    "    If false: throw to monkey 0"|]
+    [|
+    "Monkey 2:"
+    "  Starting items: 79, 60, 97"
+    "  Operation: new = old * old"
+    "  Test: divisible by 13"
+    "    If true: throw to monkey 1"
+    "    If false: throw to monkey 3"|]
+    [|
+    "Monkey 3:"
+    "  Starting items: 74"
+    "  Operation: new = old + 3"
+    "  Test: divisible by 17"
+    "    If true: throw to monkey 0"
+    "    If false: throw to monkey 1"|]|]
 
-(((example |> List.map parseMonkee |> Array.ofList), Map []), [1..20])
-||> List.fold (
-    fun (monkees, throws) _ ->
-        let monkees', throws' = round monkees
-        (monkees', mergeWith (+) throws throws'))
+
+let m0 = {
+    Items = [79; 98]
+    Operation = (*) 19
+    Test = fun x -> if x % 23 = 0 then 2 else 3 }
+let m1 = {
+    Items = [54; 64; 75; 74]
+    Operation = (+) 6
+    Test = fun x -> if x % 19 = 0 then 2 else 0 }
+let m2 = {
+    Items = [79; 60; 97]
+    Operation = fun x -> x * x
+    Test = fun x -> if x % 13 = 0 then 1 else 3 }
+let m3 = {
+    Items = [74]
+    Operation = (+) 3
+    Test = fun x -> if x % 17 = 0 then 0 else 1 }
+    
+
+let rawInput =
+    System.IO.File.ReadAllLines($"{__SOURCE_DIRECTORY__}/../input.txt")
+    
+
+let part1 monkees =
+    let (_, throws) =
+        ((monkees, Map []), [1..20])
+        ||> List.fold (
+            fun (monkees, throws) _ ->
+                let monkees', throws' = round monkees
+                (monkees', mergeWith (+) throws throws'))
+
+    throws
+    |> Map.toSeq
+    |> Seq.sortByDescending snd
+    |> Seq.take 2
+    |> Seq.map snd
+    |> Seq.reduce (*)
+            
+example
+|> Array.map parseMonkee
+|> part1
+            
+rawInput
+|> Array.chunkBySize 7
+|> Array.map (Array.take 6)
+|> Array.map parseMonkee
+|> fun x ->
+    printfn "Part 1: %d" <| part1 x
